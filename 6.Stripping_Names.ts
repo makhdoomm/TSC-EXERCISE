2a2020ea5ea15e3f867bc64b332f380909ae5e5f
makhdoom aly <alinawaz227293@gmail.com>
2024-03-27 15:50:27 +0500

    Add files via upload

---
+++ 6.Stripping_Names.ts
@@ -0,0 +1,12 @@
+
+
+const PersonName : string = '  \n\tShahid Afridi\t\n  ';
+
+console.log(PersonName);
+
+const without_Whitespaces :string = PersonName.trim();
+
+console.log(without_Whitespaces);
+
+
+
