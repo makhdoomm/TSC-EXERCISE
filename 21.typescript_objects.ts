a0322585cd0dddf81421413c87364903465ed7a6
makhdoom aly <alinawaz227293@gmail.com>
2024-03-28 13:35:16 +0500

    Add files via upload

---
+++ 21.typescript_objects.ts
@@ -0,0 +1,28 @@
+
+
+
+ let person_Name :string = 'aleey';
+
+const personName_Array :string[] = ['person' ,'car' , 'drinks'];
+
+
+interface person {
+    age : number,
+    name : string,
+    nationality : string,
+    student : boolean
+}
+
+
+
+
+
+let person  : person = {
+    
+    age : 26 ,
+    name : 'aleey',
+    nationality : 'pakistani',
+    student : true
+}
+
+console.log(person);
