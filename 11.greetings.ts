2a27ea4595cee1f0109f3150c6b5bef1f5394e3f
makhdoom aly <alinawaz227293@gmail.com>
2024-03-28 13:23:53 +0500

    Add files via upload

---
+++ 11.greetings.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['ali','naji','naym','jogi'] ;
+
+const message :string = 'Welcome to Typescript Classs: ' 
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+    
+}
