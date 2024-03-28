93deabf455ec4482a76d38e7419cb2d028d4d85e
makhdoom aly <alinawaz227293@gmail.com>
2024-03-28 13:26:15 +0500

    Add files via upload

---
+++ 13.Guest_List.ts
@@ -0,0 +1,10 @@
+
+
+let Guest_List :string[] = ['imran khan','nawaz sharif','asif zardari',];
+
+for(let i=0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr.' + Guest_List[i] + ',\n\nit is our pleasure to invite you in our party.\n\nThank You!' )
+
+    
+}
