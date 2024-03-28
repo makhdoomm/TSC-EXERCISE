50c0e666edbeff61b45de98ffc714998571238ec
makhdoom aly <alinawaz227293@gmail.com>
2024-03-28 13:36:00 +0500

    Add files via upload

---
+++ 22.international_error.ts
@@ -0,0 +1,11 @@
+
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen
+
+//cearting arrays
+let newArray :string[] = ['akash', 'mansoor', 'piyar'];
+
+//accessing array by index
+console.log(newArray[5]);
+
+// correct the error
+console.log(newArray);
