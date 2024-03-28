9dfdc825d7bfa968d54f6ac437d09c4acd53b0d8
makhdoom aly <alinawaz227293@gmail.com>
2024-03-28 13:25:09 +0500

    Add files via upload

---
+++ 12.Your Own Array.ts
@@ -0,0 +1,9 @@
+
+
+let transportation :string[] = ['motor cycle','car','train','cycle'] ;
+
+for(let i=0; i<transportation.length; i++){
+
+    console.log('i would like to own a ' + transportation[i])
+    
+}
